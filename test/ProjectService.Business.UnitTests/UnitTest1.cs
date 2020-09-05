76fa26747ad249a4884bff24ae1219a0b18964fc
Evgeniy <malkinevgeniy437@gmail.com>
2020-09-06 01:03:21 +0300

    create separated projects

---
+++ test/ProjectService.Business.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ProjectService.Business.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
