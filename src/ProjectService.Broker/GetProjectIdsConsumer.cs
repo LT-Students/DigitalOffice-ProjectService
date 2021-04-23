ff676dec836f4e232414621678de44a8b4591611
djino <malkinevgeniy437@gmail.com>
2021-04-23 12:30:39 +0300

    Add consumers

---
+++ src/ProjectService.Broker/GetProjectIdsConsumer.cs
@@ -0,0 +1,19 @@
+﻿using LT.DigitalOffice.Kernel.Broker;
+using LT.DigitalOffice.ProjectService.Models.Broker.Requests;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.ProjectService.Broker
+{
+    class GetProjectsConsumer : IConsumer<IGetProjectRequest>
+    {
+        public async Task Consume(ConsumeContext<IGetProjectRequest> context)
+        {
+            await context.RespondAsync<IOperationResult<bool>>();
+        }
+    }
+}
