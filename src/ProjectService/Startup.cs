47e7a78de2a7ed56d9eff977c39e7715eb859c9a
Mariya <mariyasin@list.ru>
2021-04-25 14:48:38 +0300

    small fix

--- src/ProjectService/Startup.cs
+++ src/ProjectService/Startup.cs
@@ -41,7 +41,7 @@ namespace LT.DigitalOffice.ProjectService
                 .GetSection(BaseRabbitMqConfig.SectionName)
                 .Get<RabbitMqConfig>();
 
-            Version = "1.1.7";
+            Version = "1.1.9";
             Description = "ProjectService is an API intended to work with projects.";
             StartTime = DateTime.UtcNow;
             ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
