cbf7272ef861986a8e75e9d67743be822ce738c0
Mariya <mariyasin@list.ru>
2021-04-30 12:39:45 +0300

    added verify to tests

--- test/ProjectService.Business.UnitTests/Commands/EditProjectCommandTests.cs
+++ test/ProjectService.Business.UnitTests/Commands/EditProjectCommandTests.cs
@@ -33,22 +33,46 @@ namespace LT.DigitalOffice.ProjectServiceUnitTests.Commands
         private JsonPatchDocument<EditProjectRequest> _request;
         private JsonPatchDocument<DbProject> _dbRequest;
         private OperationResultResponse<bool> _response;
+        private Guid _departmentId = Guid.NewGuid();
+
+        private Mock<IGetDepartmentResponse> _getDepartmentResponse;
+        private Mock<IOperationResult<IGetDepartmentResponse>> _operationResult;
+        private Mock<Response<IOperationResult<IGetDepartmentResponse>>> _brokerResponse;
 
         private IEditProjectCommand _command;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
-            _request = new JsonPatchDocument<EditProjectRequest>();
+            _request = new JsonPatchDocument<EditProjectRequest>(
+                new List<Operation<EditProjectRequest>>
+                {
+                    new Operation<EditProjectRequest>(
+                        "replace",
+                        $"/{nameof(EditProjectRequest.DepartmentId)}",
+                        "",
+                        _departmentId)
+                }, new CamelCasePropertyNamesContractResolver());
             _dbRequest = new JsonPatchDocument<DbProject>();
-            _response = new OperationResultResponse<bool>();
         }
 
         [SetUp]
         public void SetUp()
         {
+            _response = new OperationResultResponse<bool>();
             _response.Body = true;
             _response.Status = OperationResultStatusType.FullSuccess;
+
+            _getDepartmentResponse = new Mock<IGetDepartmentResponse>();
+            _getDepartmentResponse.Setup(x => x.Id).Returns(_departmentId);
+            _getDepartmentResponse.Setup(x => x.Name).Returns("Name");
+
+            _operationResult = new Mock<IOperationResult<IGetDepartmentResponse>>();
+            _operationResult.Setup(x => x.Body).Returns(_getDepartmentResponse.Object);
+            _operationResult.Setup(x => x.IsSuccess).Returns(true);
+
+            _brokerResponse = new Mock<Response<IOperationResult<IGetDepartmentResponse>>>();
+            _brokerResponse.Setup(x => x.Message).Returns(_operationResult.Object);
 
             _mocker = new AutoMocker();
             _mocker
@@ -62,6 +86,12 @@ namespace LT.DigitalOffice.ProjectServiceUnitTests.Commands
             _mocker
                 .Setup<IAccessValidator, bool>(x => x.HasRights(It.IsAny<int>()))
                 .Returns(true);
+
+            _mocker
+                .Setup<IRequestClient<IGetDepartmentRequest>, Task<Response<IOperationResult<IGetDepartmentResponse>>>>(
+                x => x.GetResponse<IOperationResult<IGetDepartmentResponse>>(
+                    IGetDepartmentRequest.CreateObj(null, _departmentId), default, default))
+                .Returns(Task.FromResult(_brokerResponse.Object));
 
             _mocker
                 .Setup<IPatchDbProjectMapper, JsonPatchDocument<DbProject>>(x =>
@@ -102,43 +132,6 @@ namespace LT.DigitalOffice.ProjectServiceUnitTests.Commands
         }
 
         [Test]
-        public void SuccessCommandWhenSuccessBrokerResponse()
-        {
-            Guid departmentId = Guid.NewGuid();
-
-            var department = new Mock<IGetDepartmentResponse>();
-            department.Setup(x => x.Id).Returns(departmentId);
-            department.Setup(x => x.Name).Returns("Name");
-
-            var operationResult = new Mock<IOperationResult<IGetDepartmentResponse>>();
-            operationResult.Setup(x => x.Body).Returns(department.Object);
-            operationResult.Setup(x => x.IsSuccess).Returns(true);
-
-            var brokerResponse = new Mock<Response<IOperationResult<IGetDepartmentResponse>>>();
-            brokerResponse.Setup(x => x.Message).Returns(operationResult.Object);
-
-            _mocker
-                .Setup<IRequestClient<IGetDepartmentRequest>, Task<Response<IOperationResult<IGetDepartmentResponse>>>>(
-                x => x.GetResponse<IOperationResult<IGetDepartmentResponse>>(
-                    IGetDepartmentRequest.CreateObj(null, departmentId), default, default))
-                .Returns(Task.FromResult(brokerResponse.Object));
-
-            var request = new JsonPatchDocument<EditProjectRequest>(
-                new List<Operation<EditProjectRequest>>
-                {
-                    new Operation<EditProjectRequest>(
-                        "replace",
-                        $"/{nameof(EditProjectRequest.DepartmentId)}",
-                        "",
-                        departmentId
-                    )
-                }, new CamelCasePropertyNamesContractResolver());
-
-            SerializerAssert.AreEqual(_response, _command.Execute(It.IsAny<Guid>(), request));
-        }
-
-
-        [Test]
         public void ValidationExceptionWhenInvalidRequest()
         {
             _mocker
@@ -146,6 +139,13 @@ namespace LT.DigitalOffice.ProjectServiceUnitTests.Commands
                 .Returns(false);
 
             Assert.Throws<ValidationException>(() => _command.Execute(It.IsAny<Guid>(), _request));
+            _mocker.Verify<IAccessValidator, bool>(x => x.IsAdmin(), Times.Never);
+            _mocker.Verify<IRequestClient<IGetDepartmentRequest>, Task<Response<IOperationResult<IGetDepartmentResponse>>>>(
+                x => x.GetResponse<IOperationResult<IGetDepartmentResponse>>(
+                    IGetDepartmentRequest.CreateObj(null, _departmentId), default, default),
+                    Times.Never);
+            _mocker.Verify<IPatchDbProjectMapper, JsonPatchDocument<DbProject>>(x => x.Map(_request), Times.Never);
+            _mocker.Verify<IProjectRepository, bool>(x => x.Edit(It.IsAny<Guid>(), It.IsAny<JsonPatchDocument<DbProject>>()), Times.Never);
         }
 
         [Test]
@@ -160,6 +160,29 @@ namespace LT.DigitalOffice.ProjectServiceUnitTests.Commands
                 .Returns(false);
 
             Assert.Throws<ForbiddenException>(() => _command.Execute(It.IsAny<Guid>(), _request));
+            _mocker.Verify<IAccessValidator, bool>(x => x.IsAdmin(), Times.Once);
+            _mocker.Verify<IRequestClient<IGetDepartmentRequest>, Task<Response<IOperationResult<IGetDepartmentResponse>>>>(
+                x => x.GetResponse<IOperationResult<IGetDepartmentResponse>>(
+                    IGetDepartmentRequest.CreateObj(null, _departmentId), default, default),
+                    Times.Never);
+            _mocker.Verify<IPatchDbProjectMapper, JsonPatchDocument<DbProject>>(x => x.Map(_request), Times.Never);
+            _mocker.Verify<IProjectRepository, bool>(x => x.Edit(It.IsAny<Guid>(), It.IsAny<JsonPatchDocument<DbProject>>()), Times.Never);
+        }
+
+        [Test]
+        public void NotSuccessBrokerResponse()
+        {
+            _operationResult.Setup(x => x.IsSuccess).Returns(false);
+            _response.Body = false;
+            _response.Status = OperationResultStatusType.Failed;
+            _response.Errors.Add("Cannot edit project. Please try again later.");
+
+            SerializerAssert.AreEqual(_response, _command.Execute(It.IsAny<Guid>(), _request));
+            _mocker.Verify<IRequestClient<IGetDepartmentRequest>, Task<Response<IOperationResult<IGetDepartmentResponse>>>>(
+                x => x.GetResponse<IOperationResult<IGetDepartmentResponse>>(
+                    IGetDepartmentRequest.CreateObj(null, _departmentId), default, default), Times.Once);
+            _mocker.Verify<IPatchDbProjectMapper, JsonPatchDocument<DbProject>>(x => x.Map(_request), Times.Never);
+            _mocker.Verify<IProjectRepository, bool>(x => x.Edit(It.IsAny<Guid>(), It.IsAny<JsonPatchDocument<DbProject>>()), Times.Never);
         }
 
         [Test]
@@ -170,6 +193,8 @@ namespace LT.DigitalOffice.ProjectServiceUnitTests.Commands
                 .Throws(new ArgumentNullException());
 
             Assert.Throws<ArgumentNullException>(() => _command.Execute(It.IsAny<Guid>(), _request));
+            _mocker.Verify<IPatchDbProjectMapper, JsonPatchDocument<DbProject>>(x => x.Map(_request), Times.Once);
+            _mocker.Verify<IProjectRepository, bool>(x => x.Edit(It.IsAny<Guid>(), It.IsAny<JsonPatchDocument<DbProject>>()), Times.Never);
         }
 
         [Test]
@@ -180,6 +205,7 @@ namespace LT.DigitalOffice.ProjectServiceUnitTests.Commands
                 .Throws(new NullReferenceException());
 
             Assert.Throws<NullReferenceException>(() => _command.Execute(It.IsAny<Guid>(), _request));
+            _mocker.Verify<IProjectRepository, bool>(x => x.Edit(It.IsAny<Guid>(), It.IsAny<JsonPatchDocument<DbProject>>()), Times.Once);
         }
     }
 }
