6c4ee2a02910c2893cfab82f822ca1b4410089d2
Mariya <mariyasin@list.ru>
2021-05-18 21:51:39 +0300

    add filter

---
+++ src/ProjectService.Models.Dto/Requests/Filters/FindDbProjectsUserFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.ProjectService.Models.Dto.Requests.Filters
+{
+    public class FindDbProjectsUserFilter
+    {
+        public Guid Id { get; set; }
+    }
+}
