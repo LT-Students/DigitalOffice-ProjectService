ced22eb2f3eecf312f9ba472693618f49c1ffd0f
Igorok <fdiii@mail.ru>
2021-07-11 22:25:28 +0300

    initial commit

---
+++ src/ProjectService.Data.Provider.MsSql.Ef/Migrations/20210711203811_FixForeignKey.cs
@@ -0,0 +1,18 @@
+using LT.DigitalOffice.ProjectService.Models.Db;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace LT.DigitalOffice.ProjectService.Data.Provider.MsSql.Ef.Migrations
+{
+    [DbContext(typeof(ProjectServiceDbContext))]
+    [Migration("20210711203811_FixForeignKey")]
+    public class FixForeignKey : Migration {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: $"FK_ProjectUser_Projects",
+                table: DbProjectUser.TableName);
+        }
+    }
+}
