67c16214f1a686bdae3e9ba48c84ef8a072825db
Mariya <mariyasin@list.ru>
2021-07-15 00:20:21 +0300

    fix migration

--- src/ProjectService.Data.Provider.MsSql.Ef/Migrations/20210714130100_AddUniqueConstraintToProjectName.cs
+++ src/ProjectService.Data.Provider.MsSql.Ef/Migrations/20210714130100_AddUniqueConstraintToProjectName.cs
@@ -10,11 +10,6 @@ namespace LT.DigitalOffice.ProjectService.Data.Provider.MsSql.Ef.Migrations
     {
         protected override void Up(MigrationBuilder builder)
         {
-            builder.AlterColumn<string>(
-                name: nameof(DbProject.Name),
-                table: DbProject.TableName,
-                maxLength: 100);
-
             builder.AddUniqueConstraint(
                 name: $"UX_Name_unique",
                 table: DbProject.TableName,
