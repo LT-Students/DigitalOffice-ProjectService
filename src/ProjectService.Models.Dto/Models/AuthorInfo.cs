b70ab1779ac5e9bca6fc6d09ae57518c5af23428
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-07-23 17:27:08 +0300

    change class ro record (#84)

--- src/ProjectService.Models.Dto/Models/AuthorInfo.cs
+++ src/ProjectService.Models.Dto/Models/AuthorInfo.cs
@@ -2,7 +2,7 @@
 
 namespace LT.DigitalOffice.ProjectService.Models.Dto.Models
 {
-    public class AuthorInfo
+    public record AuthorInfo
     {
         public Guid Id { get; set; }
         public string FirstName { get; set; }
--- src/ProjectService.Models.Dto/Models/UserTaskInfo.cs
+++ src/ProjectService.Models.Dto/Models/UserTaskInfo.cs
@@ -2,7 +2,7 @@
 
 namespace LT.DigitalOffice.ProjectService.Models.Dto.Models
 {
-    public class UserTaskInfo
+    public record UserTaskInfo
     {
         public Guid? Id { get; set; }
         public string FirstName { get; set; }
