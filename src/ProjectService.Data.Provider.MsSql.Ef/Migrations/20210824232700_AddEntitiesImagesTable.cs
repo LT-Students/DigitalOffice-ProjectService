8aba098feabd58cfca88c3f621fe4a899fbdc38f
VLiamin <LyaminVA@yandex.ru>
2021-09-13 16:26:02 +0300

    fixed migration

--- src/ProjectService.Data.Provider.MsSql.Ef/Migrations/20210824232700_AddEntitiesImagesTable.cs
+++ src/ProjectService.Data.Provider.MsSql.Ef/Migrations/20210824232700_AddEntitiesImagesTable.cs
@@ -12,22 +12,17 @@ namespace LT.DigitalOffice.ProjectService.Data.Provider.MsSql.Ef.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-               name: DbEntityImage.TableName,
-               columns: table => new
-               {
-                   Id = table.Column<Guid>(nullable: false),
-                   EntityId = table.Column<Guid>(nullable: false),
-                   ImageId = table.Column<Guid>(nullable: false)
-               },
-               constraints: table =>
-               {
-                   table.PrimaryKey("PK_EntitiesImages", x => x.Id);
-               });
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(DbEntityImage.TableName);
+                name: DbEntityImage.TableName,
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    EntityId = table.Column<Guid>(nullable: false),
+                    ImageId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntitiesImages", x => x.Id);
+                });
         }
     }
 }
