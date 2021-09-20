c9171494475d05830f0ec4eeb8957a03f7519456
VLiamin <LyaminVA@yandex.ru>
2021-09-20 13:34:08 +0300

    fixed some methods

---
+++ src/ProjectService.Mappers/Db/DbEntityImageMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LT.DigitalOffice.ProjectService.Mappers.Db.Interfaces;
+using LT.DigitalOffice.ProjectService.Models.Db;
+
+namespace LT.DigitalOffice.ProjectService.Mappers.Db
+{
+  public class DbEntityImageMapper : IDbEntityImageMapper
+  {
+    public DbEntityImage Map(Guid imageId, Guid projectId)
+    {
+      return new DbEntityImage
+      {
+        Id = Guid.NewGuid(),
+        ImageId = imageId,
+        EntityId = projectId
+      };
+    }
+  }
+}
---
+++ src/ProjectService.Mappers/Db/Interfaces/IDbEntityImageMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.ProjectService.Models.Db;
+
+namespace LT.DigitalOffice.ProjectService.Mappers.Db.Interfaces
+{
+  [AutoInject]
+  public interface IDbEntityImageMapper
+  {
+    public DbEntityImage Map(Guid imageId, Guid projectId);
+  }
+}
