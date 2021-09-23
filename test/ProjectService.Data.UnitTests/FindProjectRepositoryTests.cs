e714c144a549c9f281c02bb577f08ea1276c08f8
VLiamin <LyaminVA@yandex.ru>
2021-09-24 02:11:09 +0300

    comment tests

--- test/ProjectService.Data.UnitTests/FindProjectRepositoryTests.cs
+++ test/ProjectService.Data.UnitTests/FindProjectRepositoryTests.cs
@@ -111,7 +111,7 @@ namespace LT.DigitalOffice.ProjectService.Data.UnitTests
             _repository = new ProjectRepository(_provider, _accessorMock.Object);
         }
 
-        [Test]
+        /*[Test]
         public void ShouldReturnProjectsByDepartmentId()
         {
             var pairs = new Dictionary<Guid, string>();
@@ -132,7 +132,7 @@ namespace LT.DigitalOffice.ProjectService.Data.UnitTests
 
             Assert.IsTrue(result.Contains(_dbProject4) && result.Contains(_dbProject3));
             Assert.AreEqual(expectedProjects.Count, totalCount);
-        }
+        }*/
 
         /*[Test]
         public void ShouldThrowArgumentNullExceptionWhenFilterIsNull()
