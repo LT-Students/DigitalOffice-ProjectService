e051b5e00a88d9bd26918fe9c9d90f22f69e5f94
Mariya <mariyasin@list.ru>
2021-10-07 16:12:48 +0300

    fix

---
+++ src/ProjectService.Business/Commands/Project/Interfaces/IGetProjectCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.ProjectService.Models.Dto.Requests.Filters;
+using LT.DigitalOffice.ProjectService.Models.Dto.Responses;
+
+namespace LT.DigitalOffice.ProjectService.Business.Commands.Project.Interfaces
+{
+  public interface IGetProjectCommand
+  {
+    Task<OperationResultResponse<ProjectResponse>> Execute(GetProjectFilter filter);
+  }
+}
