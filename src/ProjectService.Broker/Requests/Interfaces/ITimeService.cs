84a8271c95e0fb6018e42b86d350006603d7389a
VLiamin <vliamin@foss.dk>
2022-04-04 15:31:17 +0300

    fixed errors

--- src/ProjectService.Broker/Requests/Interfaces/ITimeService.cs
+++ src/ProjectService.Broker/Requests/Interfaces/ITimeService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Attributes;
 
 namespace LT.DigitalOffice.ProjectService.Broker.Requests.Interfaces
 {
+  [AutoInject]
   public interface ITimeService
   {
     Task CreateWorkTimeAsync(Guid projectId, List<Guid> userIds, List<string> errors);
