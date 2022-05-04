9d4b577a706851984b2c5bcf55bd2e570b39644e
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-05-04 17:56:24 +0300

    add properties to filter

--- src/ProjectService.Models.Dto/Requests/Filters/FindProjectsFilter.cs
+++ src/ProjectService.Models.Dto/Requests/Filters/FindProjectsFilter.cs
@@ -1,8 +1,12 @@
 ﻿using LT.DigitalOffice.Kernel.Requests;
+using LT.DigitalOffice.ProjectService.Models.Dto.Enums;
 
 namespace LT.DigitalOffice.ProjectService.Models.Dto.Requests.Filters
 {
   public record FindProjectsFilter : BaseFindFilter
   {
+    public bool? IsAscendingSort { get; set; }
+    public ProjectStatusType? ProjectStatus { get; set; }
+    public string NameIncludeSubstring { get; set; }
   }
 }
