530298ab66a597551d7fc837ebe8824c234be07a
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-05-05 14:28:38 +0300

    add getting filter properties from query

--- src/ProjectService.Models.Dto/Requests/Filters/FindProjectsFilter.cs
+++ src/ProjectService.Models.Dto/Requests/Filters/FindProjectsFilter.cs
@@ -1,12 +1,18 @@
 ﻿using LT.DigitalOffice.Kernel.Requests;
 using LT.DigitalOffice.ProjectService.Models.Dto.Enums;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LT.DigitalOffice.ProjectService.Models.Dto.Requests.Filters
 {
   public record FindProjectsFilter : BaseFindFilter
   {
+    [FromQuery(Name = "isascendingsort")]
     public bool? IsAscendingSort { get; set; }
+
+    [FromQuery(Name ="projectstatus")]
     public ProjectStatusType? ProjectStatus { get; set; }
+
+    [FromQuery(Name = "nameincludesubstring")]
     public string NameIncludeSubstring { get; set; }
   }
 }
