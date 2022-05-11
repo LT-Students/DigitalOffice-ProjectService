c65a03de44ddbc083ac9b24f30d33728f8e63746
AnnPostnikova <postnikova170@yandex.ru>
2022-05-11 14:36:13 +0300

    add empty line in IUserService

--- src/ProjectService.Broker/Requests/Interfaces/IUserService.cs
+++ src/ProjectService.Broker/Requests/Interfaces/IUserService.cs
@@ -11,6 +11,7 @@ namespace LT.DigitalOffice.ProjectService.Broker.Requests.Interfaces
   public interface IUserService
   {
     Task<List<UserData>> GetUsersDatasAsync(IEnumerable<DbProjectUser> projectUsers, List<string> errors);
+
     Task<List<Guid>> CheckUsersExistenceAsync(List<Guid> usersIds, List<string> errors = null);
   }
 }
