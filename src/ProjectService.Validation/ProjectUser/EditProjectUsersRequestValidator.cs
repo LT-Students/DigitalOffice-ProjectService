7b70c6d0f7223b60c150ea5a5eec84ad9b98b39d
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-05-12 21:05:06 +0300

    add request validator

---
+++ src/ProjectService.Validation/ProjectUser/EditProjectUsersRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.ProjectService.Models.Dto.Requests;
+using LT.DigitalOffice.ProjectService.Validation.ProjectUser.Interfaces;
+
+namespace LT.DigitalOffice.ProjectService.Validation.ProjectUser
+{
+  public class EditProjectUsersRequestValidator : AbstractValidator<EditProjectUsersRequest>, IEditProjectUsersRequestValidator
+  {
+    public EditProjectUsersRequestValidator()
+    {
+      
+    }
+  }
+}
