56daa596d156b217164e542f0d679178e7ba355c
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-05-12 14:18:17 +0300

    add request

---
+++ src/ProjectService.Models.Dto/Requests/EditProjectUsersRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.ProjectService.Models.Dto.Requests
+{
+  public record EditProjectUsersRequest
+  {
+    public List<Guid> UsersIds { get; set; }
+    public List<int> RoleTypes { get; set; }
+  }
+}
