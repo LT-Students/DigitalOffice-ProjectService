2894c11d08a3632e05f10336b451fc2d15451315
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-05-12 14:18:07 +0300

    add command

---
+++ src/ProjectService.Business/Commands/ProjectUsers/EditProjectUsersCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.ProjectService.Business.Commands.ProjectUsers.Interfaces;
+using LT.DigitalOffice.ProjectService.Models.Dto.Requests;
+
+namespace LT.DigitalOffice.ProjectService.Business.Commands.ProjectUsers
+{
+  internal class EditProjectUsersCommand : IEditProjectUsersCommand
+  {
+    public Task<OperationResultResponse<bool>> ExecuteAsync(EditProjectUsersRequest request)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ src/ProjectService.Business/Commands/ProjectUsers/Interfaces/IEditProjectUsersCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.ProjectService.Models.Dto.Requests;
+
+namespace LT.DigitalOffice.ProjectService.Business.Commands.ProjectUsers.Interfaces
+{
+  [AutoInject]
+  public interface IEditProjectUsersCommand
+  {
+    Task<OperationResultResponse<bool>> ExecuteAsync(EditProjectUsersRequest request);
+  }
+}
