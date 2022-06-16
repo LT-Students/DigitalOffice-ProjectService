475c659bdd4b0cde18bda2f3b7e2bf780f85f155
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-06-16 17:26:27 +0300

    Feature/add remove images to publish (#171)

--- src/ProjectService.Broker/Publishes/Interfaces/IPublish.cs
+++ src/ProjectService.Broker/Publishes/Interfaces/IPublish.cs
@@ -10,5 +10,6 @@ namespace LT.DigitalOffice.ProjectService.Broker.Publishes.Interfaces
   {
     Task CreateDepartmentEntityAsync(Guid departmentId, Guid createdBy, Guid projectId);
     Task CreateWorkTimeAsync(Guid projectId, List<Guid> usersIds);
+    Task RemoveImagesAsync(List<Guid> imagesIds);
   }
 }
