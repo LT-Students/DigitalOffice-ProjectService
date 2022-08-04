bd6c38d94aa8c8d82deaa7c59b6749db40361e7d
Masha <mariyasin@list.ru>
2022-08-04 13:32:47 +0300

    fixed create project mapper

--- src/ProjectService.Models.Dto/Requests/CreateProjectRequest.cs
+++ src/ProjectService.Models.Dto/Requests/CreateProjectRequest.cs
@@ -15,7 +15,7 @@ namespace LT.DigitalOffice.ProjectService.Models.Dto.Requests
     public string Description { get; set; }
     public string ShortDescription { get; set; }
     public string Customer { get; set; }
-    public DateTime? StartDateUtc { get; set; }
+    public DateTime StartDateUtc { get; set; }
     public DateTime? EndDateUtc { get; set; }
     public Guid? DepartmentId { get; set; }
     public ProjectStatusType Status { get; set; }
