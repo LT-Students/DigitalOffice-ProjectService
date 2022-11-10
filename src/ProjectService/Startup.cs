4f97779ea65970434addc0f43da48e7b7ab7d02d
sparrowofnight@gmail.com <sparrowofnight@gmail.com>
2022-11-11 01:43:36 +0300

    fix version

--- src/ProjectService/Startup.cs
+++ src/ProjectService/Startup.cs
@@ -59,7 +59,7 @@ namespace LT.DigitalOffice.ProjectService
         .GetSection(BaseRabbitMqConfig.SectionName)
         .Get<RabbitMqConfig>();
 
-      Version = "1.2.3.4";
+      Version = "1.2.3.5";
       Description = "ProjectService is an API intended to work with projects.";
       StartTime = DateTime.UtcNow;
       ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
